60092aee308b515bb02214044405e61260ebdfec
Zanele01 <0706885n@students.wits.ac.za>
2018-07-31 15:48:08 +0200

    Updated graph drawing

--- FamilyRelationshipGraph/graphTrial.py
+++ FamilyRelationshipGraph/graphTrial.py
@@ -5,7 +5,7 @@ graph = nx.path_graph(5) #graph corresponding to lines in adjacency list format
 graph = nx.read_adjlist("data.txt",comments='#',delimiter=None,create_using=nx.Graph(),nodetype=float,encoding='utf-8')
 
 pos = nx.random_layout(graph)
-nx.draw_networkx_nodes(graph, pos, node_size = 200)
+nx.draw(graph)
 
 plt.axis('off')
 plt.show()

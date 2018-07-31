3e34413e64553f6b0b142e2b54fd8d45376c9c52
Zanele01 <0706885n@students.wits.ac.za>
2018-07-31 15:32:40 +0200

    Changed node output

--- FamilyRelationshipGraph/graphTrial.py
+++ FamilyRelationshipGraph/graphTrial.py
@@ -2,7 +2,6 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 graph = nx.path_graph(5) #graph corresponding to lines in adjacency list format
-nx.write_adjlist(graph,"data.txt")
 graph = nx.read_adjlist("data.txt",comments='#',delimiter=None,create_using=nx.Graph(),nodetype=float,encoding='utf-8')
 
 pos = nx.random_layout(graph)

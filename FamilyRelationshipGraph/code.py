a7c2a4ae009d385a203dda04b67a618502442047
Tshembani Sibuyi <838214@students.wits.ac.za>
2018-07-31 13:31:12 +0200

    import data

---
+++ FamilyRelationshipGraph/code.py
@@ -0,0 +1,19 @@
+import networkx as nx
+from collections import defaultdict
+import matplotlib.pyplot as plt
+
+#read relationships and store in an adjacency list
+def create_list(adj):
+	data = open('data.txt', 'r').readlines()
+	for line in data:
+		u,v,w = map(float, line.strip().split(' '))
+		adj[u].append((v,w))
+		adj[v].append((u,w))	
+	return adj
+	
+if __name__ == '__main__':
+
+	adj = defaultdict(list)
+	adj_list = create_list(adj)
+
+	g = nx.Graph()

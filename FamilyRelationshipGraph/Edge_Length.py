19684a9bade2609ee566b9e3c7a84277c50f4ed5
Zanele01 <0706885n@students.wits.ac.za>
2018-08-07 13:10:43 +0200

    Updated Edge Length Class

--- FamilyRelationshipGraph/Edge_Length.py
+++ FamilyRelationshipGraph/Edge_Length.py
@@ -3,15 +3,14 @@ import matplotlib.pyplot as plt
  
 class Edge_Length:
  	
-	def __init__(self,graph,weight,length):
-		self.G = graph
-		self.edge_weight = weight
-		self.edge_length = length
+	def __init__(self):
+		self.edge_length = []
 
-	def convert_edge_weights_to_length(Graph, weight_value):
-		edge_length = 1 - weight_value
-		return edge_length
+	def convert_edge_weights_to_length(self, graph):
+		for u,v,w in graph.edges(data='weight'):
+			self.edge_length.append(1 - w)
 		
 	def get_edge(self):
 		return self.edge_length
 
+	

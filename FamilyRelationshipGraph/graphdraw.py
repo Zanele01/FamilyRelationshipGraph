2d0b9cdc3ffe9421e7737c84ef9647b91890d7f9
Tshembani Sibuyi <838214@students.wits.ac.za>
2018-08-10 12:17:04 +0200

    graphdrawclass

--- FamilyRelationshipGraph/graphdraw.py
+++ FamilyRelationshipGraph/graphdraw.py
@@ -1,9 +1,5 @@
-#libraries
-import networkx as nx
-import pandas as pd
 import matplotlib.pyplot as plt
-from Colormap import Colormap
-from Edge_Length import Edge_Length
+import networkx as nx
 
 class Graph_draw:
     #Initializer
@@ -30,24 +26,3 @@ class Graph_draw:
         nx.draw(graph,pos=None, node_size=150,with_labels=True,node_color = map['features'].cat.codes,cmap=plt.cm.get_cmap("Set1"), edge_color = 'gray')
         plt.axis('off')
         plt.show()
-
-if __name__ == '__main__':
-
-    #create networkx graph
-    graph = nx.Graph()
-    G = Graph_draw() #create graph draw object
-    G.create_edgelist('data.txt')
-
-    #calculate edge length
-    length = Edge_Length()
-    length.convert_edge_weights_to_length(G.get_weights())
-        
-    #add colormap 
-    Colormap = Colormap()
-    Colormap.create_nodes('nodes.txt')
-    df = pd.DataFrame()
-
-    G.add_edges(graph, length.get_length()) #add all edges to graph
-    df = Colormap.create_map(graph)
-
-    G.draw_graph(graph,df)
---
+++ FamilyRelationshipGraph/main.py
@@ -0,0 +1,27 @@
+#libraries
+import networkx as nx
+import pandas as pd
+from graphdraw import Graph_draw
+from Colormap import Colormap
+from Edge_Length import Edge_Length
+
+if __name__ == '__main__':
+
+    #create networkx graph
+    graph = nx.Graph()
+    G = Graph_draw() #create graph draw object
+    G.create_edgelist('data.txt')
+
+    #calculate edge length
+    length = Edge_Length()
+    length.convert_edge_weights_to_length(G.get_weights())
+        
+    #add colormap 
+    Colormap = Colormap()
+    Colormap.create_nodes('nodes.txt')
+    df = pd.DataFrame()
+
+    G.add_edges(graph, length.get_length()) #add all edges to graph
+    df = Colormap.create_map(graph)
+
+    G.draw_graph(graph,df)

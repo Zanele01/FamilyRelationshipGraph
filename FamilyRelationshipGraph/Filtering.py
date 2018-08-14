b6ae328e061293027905305950268463e406cf35
Zanele01 <0706885n@students.wits.ac.za>
2018-08-14 19:10:13 +0200

    User Input Functionality

--- FamilyRelationshipGraph/Filtering.py
+++ FamilyRelationshipGraph/Filtering.py
@@ -3,23 +3,26 @@ import networkx as nx
 
 class Filtering:
     
-    def __init__(self):
-        self.attributes = []
+	def __init__(self):
+		self.attributes = 0
 		
-	def select_node_data(self, node_attribute):
-		node_attribute = input("Age to be viewed on screen:")
-		self.attributes.append(node_attribute)
+	def select_node_data(self):
+		node_attribute = input("Feauture to be viewed on screen:")
+		self.attributes = int(node_attribute)
 	
 		try:
 			selection = int(node_attribute)
 		except ValueError:
 			print("Invalid selection")
-			
-	def get_graph_data(self):
+		
+	def get_node_data(self):
 		return self.attributes
 		
-	def display_graph_data(self, graph):
-		for attributes in graph.nodes(data = True):
-				print (attributes)
+	
+	
+		
+
+	
+	
 	
 	
--- FamilyRelationshipGraph/graphdraw.py
+++ FamilyRelationshipGraph/graphdraw.py
@@ -1,33 +1,48 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
+from Filtering import Filtering
+
 class Graph_draw:
-    #Initializer
-    def __init__(self):
-        self.edges = []
-        self.weights = []
 
-    def create_edgelist(self, filename):
-        data = open(filename, 'r')
-        lines = data.readlines()
-        for line in lines:
-            u,v,w = map(float, line.strip().split(' '))   
-            self.edges.append((int(u),int(v)))    
-            self.weights.append(w)
-        data.close()
-
-    def get_weights(self):
-        return self.weights
-
-    def get_edges(self):
-        return self.edges
-
-    def add_edges(self, graph, length):
-        edge_size = len(self.edges)
-        for i in range(0,edge_size):
-            graph.add_edge(self.edges[i][0], self.edges[i][1], weight= self.weights[i], length = length[i]) #add edges to graph
+	def __init__(self):
+		self.edges = []
+		self.weights = []
+		self.filter = []
+		
+	def create_edgelist(self, filename):
+		data = open('data.txt', 'r').readlines()
+		for line in data:
+			u,v,w = map(float, line.strip().split(' '))   
+			self.edges.append((int(u),int(v)))    
+			self.weights.append(w)
+			
+	def get_weights(self):
+		return self.weights
+		
+	def add_edges(self, graph, length):
+		edge_size = len(self.edges)
+		for i in range(0, edge_size):
+			graph.add_edge(self.edges[i][0], self.edges[i][1], weight = self.weights[i], length = length[i]) #add edges to graph
+			
+	def search_node_attributes(self,graph):
+		Filter = Filtering()
+		Filter.select_node_data()
+		length = len(graph.nodes())
+		for i in range(0,length):
+			if graph.nodes[i]['feature'] > Filter.get_node_data():
+				self.filter.append(graph.nodes[i]['feature'])
+		for j in self.filter:
+			print(j)
+					
+	def draw_graph(self, graph, map):
+		self.search_node_attributes(graph)
+		nx.draw(graph, pos = None, node_size = 150, with_labels = True, node_color = map['features'].cat.codes, cmap = plt.cm.get_cmap("Set1"), edge_color = 'gray')
+		plt.axis('off')
+		plt.show()
+		
 	
-    def draw_graph(self, graph, map):
-        nx.draw(graph,pos=None, node_size=150,with_labels=True,node_color = map['features'].cat.codes,cmap=plt.cm.get_cmap("Set1"), edge_color = 'gray')
-        plt.axis('off')
-        plt.show()
+		
+		
+	
+		
